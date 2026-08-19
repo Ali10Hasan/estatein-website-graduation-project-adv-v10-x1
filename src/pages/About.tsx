76850d897f841e2612fccb02892772176634cd3a
Aya <aya970525@gmail.com>
2026-08-19 22:29:19 +0300

    feat:add container && our value section and our value clients in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
+import { OurValues } from '../features/about/sections/OurValues';
+import OurValuedClients from '../features/about/sections/OurValuedClients';
 
-const About = () => {
-    return (
-        <div>About</div>
-    )
-}
+export const About: React.FC = () => {
+  return (
+    <main className="bg-[#141414] min-height-screen">
+      <OurValues />
+      <OurValuedClients />
+    </main>
+  );
+};
 
-export default About
+export default About;
