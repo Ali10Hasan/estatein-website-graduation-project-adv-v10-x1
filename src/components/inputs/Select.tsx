48f4e0a0b9594f73f3ffc175fdc22b3b1a90212e
Ali000Hasan <ah630603@gmail.com>
2026-08-24 23:12:38 -0700

    The Filter For Select And Search

---
+++ src/components/inputs/Select.tsx
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateFilter } from "../../redux/slices/propertiesSlice";
+import type { RootState } from "../../redux/store/store";
+import { IoIosArrowDown } from "react-icons/io";
+import type { JSX } from "react";
+interface SelectProps {
+  Icone: JSX.Element;
+  FilterText: string;
+  filterKey: keyof RootState["properties"]["filters"];
+  options: string[]; 
+}
+
+const Select = ({ Icone, FilterText, filterKey, options }: SelectProps) => {
+  const dispatch = useDispatch();
+  
+  
+  const selectedValue = useSelector((state: RootState) => state.properties.filters[filterKey]);
+
+  return (
+    <div className="filter-info flex md:w-[30%] lg:items-center w-full md:justify-around p-20 md:p-2 md:py-15 lg:px-10 lg:py-25 bg-smoky-black rounded-[10px] gap-5 md:gap-3 lg:gap-5">
+      <div className="icone text-[30px] md:text-[25px] lg:text-[30px] text-grey-20">
+        {Icone}
+      </div>
+   
+      <div className="relative w-full flex">
+        <select 
+          className="w-full bg-transparent text-white text-lg md:text-[12px] lg:text-[16px] outline-none border-none appearance-none cursor-pointer"
+          value={selectedValue}
+       
+          onChange={(e) => dispatch(updateFilter({ key: filterKey as any, value: e.target.value }))}
+        >
+          <option value="" disabled hidden>{FilterText}</option>
+          
+         
+          <option value="" className="bg-grey-10 text-grey-40">All {FilterText}s</option>
+
+          {options.map((optionValue, idx) => (
+            <option key={idx} value={optionValue} className="bg-grey-10">
+              {optionValue}
+            </option>
+          ))}
+        </select>
+        
+        <div className="absolute h-30 w-30 md:w-20 md:h-20 lg:w-30 lg:h-30 bg-grey-20 rounded-[50%] md:top-2 lg:top-auto right-0 md:right-1 lg:right-1 flex items-center justify-center text-grey-40 pointer-events-none">
+          <IoIosArrowDown />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Select;
