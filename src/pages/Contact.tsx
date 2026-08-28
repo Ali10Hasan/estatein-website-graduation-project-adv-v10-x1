c0d1cab4780864c1c4225f338a0ceb22674599ee
saba-barbar <barbarsaba@gmail.com>
2026-08-28 22:21:47 +0300

    fix background of stars

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,6 @@
 import Form from "../components/Form/Form"
-
 import PageHero from "../components/Hero/PageHero"
-import ExploreEstatein from "../components/sections/ExploreEstatein"
+
 
 
 const Contact = () => {
@@ -11,10 +10,9 @@ const Contact = () => {
                 title={"Get in Touch with Estatein"}
                 description={"Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation."}
                 className="2xl:h-450 xl:h-345 h-257"
-           
+
             />
             <Form />
-<ExploreEstatein/>
         </div>
     )
 }
