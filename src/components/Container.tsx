da1f835b4deb89563e8486032e7f643ca5e840ff
saba-barbar <barbarsaba@gmail.com>
2026-08-29 21:36:47 +0300

    fix: container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface containerProps{
 
 const Container = ({children , className}: containerProps) => {
   return (
-    <div className={`w-full max-w-1900 mx-auto px-16 sm:px-32 md:px-48  lg:px-64 xl:px-80 2xl:px-160 pb-80  md:pb-120 lg:pb-150 ${className}`}>
+    <div className={`w-full max-w-1900 mx-auto px-16 sm:px-32 md:px-48 lg:px-64 xl:px-80 2xl:px-160 pb-80 md:pb-120 lg:pb-150   ${className}`}>
       {children}
     </div>
   );
